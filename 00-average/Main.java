7a2241430fc464a240326924bcc86983c93bb6d6
Wai <Mincl5792@gmail.com>
2020-10-26 09:47:02 -0400

    *same as before but for average.java

--- 00-average/Main.java
+++ 00-average/Main.java
@@ -6,17 +6,17 @@ class Main {
 
     public static void main(String[] args)  {
    Scanner sc=new Scanner(System.in);
-   int y = 0; //bucket
+   int totalGrades = 0; //bucket
    int i = 0;
    System.out.println("# of grades?");
-   int z = sc.nextInt();
-   while ( i < z) {
+   int numGrades = sc.nextInt();
+   while ( i < numGrades) {
        System.out.println("# " + Math.floor(i + 1) + " Enter your grade.");
-   y+= sc.nextInt();
+   totalGrades+= sc.nextInt();
   i++;
 }
 
-  int fin = (y/z);
+  int fin = (totalGrades/numGrades);
 
   System.out.println("Your Average is: " + fin);
 
