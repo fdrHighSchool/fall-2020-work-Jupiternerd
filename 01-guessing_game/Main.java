99390a14acfbba198f04291123e4f417fe315fd6
Wai <Mincl5792@gmail.com>
2020-10-26 09:33:06 -0400

    edited tempconverter to fix reviews.

--- 01-guessing_game/Main.java
+++ 01-guessing_game/Main.java
@@ -6,70 +6,71 @@ import java.util.function.Function;
 import java.lang.*;
 
 class Main {
-  public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-    Setup(sc);
-  }
-public static void Begin(String FnL, Integer fInt, Integer cInt, Integer truth, Scanner sc) {
-int shotInt = 0;
-int tries = 0;
-while (shotInt != truth) {
-  System.out.println("[ computer ] Take your shot!");
+    public static void main(String[] args) {
 
-  shotInt = sc.nextInt();
-if (shotInt == truth) {
-  tries += 1;
- System.out.println("[ computer ] You got it!\nThe answer was: " + truth + "\nTook you: " + "[" + tries +"]" + " tries!\n =========Play again? [y]=========");
+        Setup();
+    }
+    public static void Begin(String FnL, Integer fInt, Integer cInt, Integer truth) {
+        Scanner sc = new Scanner(System.in);
+        int shotInt = 0;
+        int tries = 0;
+        while (shotInt != truth) {
+            System.out.println("[ computer ] Take your shot!");
 
-  String retryO = sc.next();
+            shotInt = sc.nextInt();
+            if (shotInt == truth) {
+                tries += 1;
+                System.out.println("[ computer ] You got it!\nThe answer was: " + truth + "\nTook you: " + "[" + tries + "]" + " tries!\n =========Play again? [y]=========");
 
-if (retryO.contains("y")) Setup(sc);
+                String retryO = sc.next();
 
-} else {
+                if (retryO.equalsIgnoreCase("y")) Setup();
 
-  System.out.println("[ computer ] Nope, hint: ");
+            } else {
 
-if (truth % 2 == 0 ) {
-  System.out.println("[ computer ] its even.");
-} else {
-System.out.println("[ computer ] its odd.");
-}
-tries += 1;
-}
-  //System.out.println(shotInt);
-}
-/*
+                System.out.println("[ computer ] Nope, hint: ");
 
-System.out.println("Take a shot!" + fInt+" - "+cInt);
+                if (truth % 2 == 0) {
+                    System.out.println("[ computer ] its even.");
+                } else {
+                    System.out.println("[ computer ] its odd.");
+                }
+                tries += 1;
+            }
+            //System.out.println(shotInt);
+        }
+        /*
 
-Integer shots = sc.nextInt();
+        System.out.println("Take a shot!" + fInt+" - "+cInt);
 
-  System.out.println("Workds" + truth);
-  */
-}
+        Integer shots = sc.nextInt();
 
-    public static void Setup(Scanner sc) {
+          System.out.println("Workds" + truth);
+          */
+    }
 
-    System.out.println("[ computer ] Hello, input your nick name.");
-String FnL = sc.next(); //Gets name.
+    public static void Setup() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("[ computer ] Hello, input your nick name.");
+        String FnL = sc.next(); //Gets name.
 
-System.out.println("[ computer ] Hey " + FnL + ", Choose the # floor of the game.");
-Integer fInt = sc.nextInt();
-System.out.println("[ computer ] Now choose the # ceiling of the game.");
-Integer cInt = sc.nextInt();
-System.out.println("[ computer ] Great " + FnL + ", heres your range:  " + fInt + " - " + cInt);
+        System.out.println("[ computer ] Hey " + FnL + ", Choose the # floor of the game.");
+        Integer fInt = sc.nextInt();
+        System.out.println("[ computer ] Now choose the # ceiling of the game.");
+        Integer cInt = sc.nextInt();
+        System.out.println("[ computer ] Great " + FnL + ", heres your range:  " + fInt + " - " + cInt);
 
-System.out.println("[y/n]");
+        System.out.println("[y/n]");
 
-String resetO = sc.next();
+        String resetO = sc.next();
 
-if (resetO.contains("y")) {
-  Integer truth = (int)(Math.random() * (cInt - fInt + 1) + fInt);
-Begin(FnL, fInt, cInt, truth, sc);
-} else {
-Setup(sc);
-}
+        if (resetO.equalsIgnoreCase("y")) {
+            Integer truth = (int)(Math.random() * (cInt - fInt + 1) + fInt);
+            Begin(FnL, fInt, cInt, truth);
+        } else {
+            Setup();
+        }
 
 
-  }
+    }
 }
